58891d0f6b2974a0f525768b53fb938d9fd85c20
Smriti Das <dassmrit@gmail.com>
2018-01-12 15:43:14 -0500

    Update to include API ID and API Key.

--- app/src/main/java/com/example/android/recipes/Constants.java
+++ app/src/main/java/com/example/android/recipes/Constants.java
@@ -1,8 +1,9 @@
 package com.example.android.recipes;
 
-
-
 public class Constants {
+
+    public static final String APP_KEY = BuildConfig.APP_KEY;
+    public static final String APP_ID = BuildConfig.APP_ID;
 
 
 }
