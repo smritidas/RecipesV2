5258137bce0ef74f7e4b2ef2940de7888a7760d6
Smriti Das <dassmrit@gmail.com>
2018-01-18 18:54:11 -0500

    Update RecipeActivity to use RecyclerView.

--- app/src/main/java/com/example/android/recipes/ui/RecipesActivity.java
+++ app/src/main/java/com/example/android/recipes/ui/RecipesActivity.java
@@ -3,6 +3,8 @@ package com.example.android.recipes.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 
 
 import com.example.android.recipes.R;
+import com.example.android.recipes.adapters.RecipeListAdapter;
 import com.example.android.recipes.models.Recipes;
 import com.example.android.recipes.services.RecipeService;
 
@@ -25,8 +28,8 @@ import okhttp3.Response;
 public class RecipesActivity extends AppCompatActivity {
     public static final String TAG = RecipesActivity.class.getSimpleName();
 
-    @BindView(R.id.recipeTextView) TextView recipeTextView;
-    @BindView(R.id.listView) ListView listView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RecipeListAdapter mAdapter;
 
     public ArrayList<Recipes> recipes = new ArrayList<>();
 
@@ -38,47 +41,35 @@ public class RecipesActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String ingredient = intent.getStringExtra("ingredient");
-
-        recipeTextView.setText("Here are all the restaurants near: " + ingredient);
         getRecipes(ingredient);
     }
 
-    private void getRecipes(String ingredient){
+    private void getRecipes(String ingredient) {
         final RecipeService recipeService = new RecipeService();
         recipeService.findRecipes(ingredient, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                    e.printStackTrace();
+                e.printStackTrace();
             }
 
             @Override
             public void onResponse(Call call, Response response) {
-                    recipes = recipeService.processResults(response);
+                recipes = recipeService.processResults(response);
 
-                    RecipesActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            String[] recipeNames =  new String[recipes.size()];
-                            for (int i = 0; i < recipeNames.length; i++ ){
-                                recipeNames[i] = recipes.get(i).getName();
-                            }
-                            ArrayAdapter adapter = new ArrayAdapter(RecipesActivity.this,
-                                    android.R.layout.simple_list_item_1, recipeNames);
-                                    listView.setAdapter(adapter);
+                RecipesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new RecipeListAdapter(getApplicationContext(), recipes);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(RecipesActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
 
-                                    for (Recipes recipe : recipes){
-                                        Log.d(TAG, "Name: " + recipe.getName());
-                                        Log.d(TAG, "Image: " + recipe.getImageURL());
-                                        Log.d(TAG, "URL: " + recipe.getUrl());
-                                        Log.d(TAG, "Servings: " + Integer.toString(recipe.getNoOfServings()));
-                                        Log.d(TAG, "Ingredients: " + android.text.TextUtils.join(", ", recipe.getIngredients()));
-                                    }
-                        }
-                    });
-
-                }
+                });
+            }
 
         });
     }
-
 }
