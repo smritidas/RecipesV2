eb9483c47fa2e25ab5d203d5a34cd7aaed7e8d1b
Smriti Das <dassmrit@gmail.com>
2018-01-30 16:35:30 -0500

    Add onCreate to RecipeDetailActivity

--- app/src/main/java/com/example/android/recipes/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/example/android/recipes/ui/RecipeDetailActivity.java
@@ -1,5 +1,37 @@
 package com.example.android.recipes.ui;
 
 
-public class RecipeDetailActivity {
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.recipes.R;
+import com.example.android.recipes.adapters.RecipePagerAdapter;
+import com.example.android.recipes.models.Recipes;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipeDetailActivity extends AppCompatActivity{
+   @BindView(R.id.viewPager) ViewPager mViewPager;
+   private RecipePagerAdapter adapterViewPager;
+   ArrayList<Recipes> mRecipes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+        ButterKnife.bind(this);
+
+        mRecipes = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new RecipePagerAdapter(getSupportFragmentManager(), mRecipes);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+    }
 }
